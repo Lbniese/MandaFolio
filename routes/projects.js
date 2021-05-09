5e44493ce5cde63642b1851594758bd454b8de8c
Lbniese <Lbniese@users.noreply.github.com>
2021-05-09 16:43:15 +0200

    Added more projects

--- routes/projects.js
+++ routes/projects.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 
 const projects = [{
+  title: 'Assignment-Dashboard',
+  description: 'Website containing a CRUD Dashboard to display and manage Assignments',
+  startDate: new Date('2020-12-01'),
+  endDate: new Date('2021-01-27'),
+  gitLink: 'https://github.com/Lbniese/Assignment-Dashboard',
+},
+{
   title: 'KS-TOTALSERVICE',
   description: 'Company site for the company KS-TOTALSERVICE',
   startDate: new Date('2020-12-07'),
@@ -14,12 +21,20 @@ const projects = [{
   endDate: new Date('2021-05-05'),
   gitLink: 'https://github.com/Lbniese/PWGen-NodeJSPowered',
 },
+
 {
   title: 'Assignment-Dashboard',
   description: 'Website containing a CRUD Dashboard to display and manage Assignments',
   startDate: new Date('2020-12-01'),
   endDate: new Date('2021-01-27'),
   gitLink: 'https://github.com/Lbniese/Assignment-Dashboard',
+},
+{
+  title: 'Todo API',
+  description: 'CRUD API for Assignments and Todos - Includes Frontend Webpage',
+  startDate: new Date('2020-10-23'),
+  endDate: new Date('2020-10-23'),
+  gitLink: 'https://github.com/Lbniese/TodoApi',
 },
 ];
 
